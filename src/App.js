7a9f08cc3d845ae299df1811eb5518fa005d6ef0
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2022-01-08 17:50:46 +0530

    broke up App into subcomponents

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 
+import Avatar from '@mui/material/Avatar';
+import AvatarGroup from '@mui/material/AvatarGroup';
 import Box from '@mui/material/Box';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Button from '@mui/material/Button';
@@ -11,11 +13,13 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import LockIcon from '@mui/icons-material/Lock';
 
 import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import LiveTvRoundedIcon from '@mui/icons-material/LiveTvRounded';
 import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 function Sidebar(props) {
   return (<Drawer
@@ -30,9 +34,9 @@ function Sidebar(props) {
       },
     }}
   >
-    <div style={{ backgroundImage: 'url("nav-header.png")', backgroundSize: "cover", display: "flex", flexDirection: "column", alignItems: "center" }}>
-      <h3 style={{ margin: "1em" }}>Functional Strength <br /> Training with Kara</h3>
-      <LinearProgressWithLabel sx={{ width: "100%" }} variant="determinate" value="10%" />
+    <div style={{ backgroundImage: 'url("nav-header.png")', backgroundSize: "cover", padding: "1em" }}>
+      <h3>Functional Strength <br /> Training with Kara</h3>
+      <LinearProgressWithLabel sx={{ width: "100%" }} color="secondary" variant="determinate" value={80} />
     </div>
     <List>
       <ListItem button key="Dashboard">
@@ -57,6 +61,83 @@ function Sidebar(props) {
     </List>
     <Divider />
   </Drawer>);
+}
+
+function Content(props) {
+
+  function VideoThumbnail(props) {
+    return (
+      <div style={{ marginRight: "2em", display: "flex", flexDirection: "column" }}>
+        <article style={{ display: "contents" }} className="box">
+          <img src="stretching.jpg" style={{ width: "10em" }} className="img" />
+        </article>
+        <LinearProgressWithLabel variant="determinate" value={50} />
+      </div>
+    );
+  }
+
+  function VideoCard(props) {
+    return (
+      <ListItem>
+        <VideoThumbnail />
+        <ListItemText style={{ maxWidth: "30em" }}>
+          <b>Day {props.index} | {props.title}</b> <br /> {props.description}
+        </ListItemText>
+        {(props.completed) ? <Button startIcon={<PlayArrowIcon />}>Watch Again</Button> : <Button>Completed</Button>}
+      </ListItem>
+    );
+  }
+
+  return (
+    <Box component="main" sx={{ textAlign: "justify", margin: "4em" }}>
+      <div>
+        <Breadcrumbs aria-label="breadcrumb" separator=">">
+          <span style={{ display: "flex", alignItems: "center" }}>
+            Week 1
+            <HomeRoundedIcon style={{ margin: "0.10em" }} fontSize="inherit" />
+          </span>
+          <span style={{ display: "flex", alignItems: "center" }}>
+            Week 2
+            <LockIcon style={{ margin: "0.10em" }} fontSize="inherit" />
+          </span>
+          <span style={{ display: "flex", alignItems: "center" }}>
+            Week 3
+            <LockIcon style={{ margin: "0.10em" }} fontSize="inherit" />
+          </span>
+          <span style={{ display: "flex", alignItems: "center" }}>
+            Week 4
+            <LockIcon style={{ margin: "0.10em" }} fontSize="inherit" />
+          </span>
+        </Breadcrumbs>
+        <div>
+          <AvatarGroup max={3}>
+            <Avatar src="stretching.jpg" />
+            <Avatar src="stretching.jpg" />
+            <Avatar src="stretching.jpg" />
+          </AvatarGroup>
+          <span><em>You and 50+ others are here</em></span>
+        </div>
+      </div>
+      <h1>💪🏻 Week 1 | Form and Technique</h1>
+      <List>
+        {props.courseItems.map((card, index) => (
+          <VideoCard index={index + 1} {...card} />
+        ))}
+      </List>
+    </Box>
+  );
+}
+
+function Hero() {
+  return (
+    <div style={{ height: "27em", backgroundImage: 'url("unsplash_h4i9G-de7Po.png")', backgroundSize: "cover", backgroundRepeat: "no-repeat", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+      <div>
+        <h2>Week 1 | Form and Technique</h2>
+        <p><em>The World's Fittest Woman Kara Saunders trains you for the next <br /> eight weeks to build strength, mobility and endurance in this series.</em></p>
+        <Button>Resume</Button>
+      </div>
+    </div>
+  );
 }
 
 function App() {
@@ -94,30 +175,15 @@ function App() {
     },
   ]
 
-  const drawerWidth = 240;
+  const drawerWidth = 280;
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <Sidebar drawerWidth={drawerWidth} />
-      <Box component="main" sx={{ textAlign: "justify", flexGrow: 1, margin: 0 }}>
-        <div style={{ height: "100%", backgroundImage: 'url("unsplash_h4i9G-de7Po.png")', backgroundSize: "cover", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-          <div style={{ margin: "5em" }}>
-            <h2>Week 1 | Form and Technique</h2>
-            <p><em>The World's Fittest Woman Kara Saunders trains you for the next <br /> eight weeks to build strength, mobility and endurance in this series.</em></p>
-            <Button>Resume</Button>
-          </div>
-        </div>
-        <div>
-          <Breadcrumbs aria-label="breadcrumb">
-            <span>Week 1</span>
-            <span>Week 2</span>
-            <span>Week 3</span>
-            <span>Week 4</span>
-          </Breadcrumbs>
-          <p><em>You and 50+ others are here</em></p>
-        </div>
-        <h1>💪🏻 Week 1 | Form and Technique</h1>
-      </Box>
+      <div style={{ flexGrow: 1 }}>
+        <Hero />
+        <Content courseItems={courseItems} />
+      </div>
     </Box>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ ReactDOM.render(
   <ThemeProvider
     theme={createTheme({
       palette: {
+        secondary: {
+          main: "rgb(255,0,107;",
+          mainGradient: "linear-gradient(180deg, rgba(255,0,107,1) 0%, rgba(165,0,243,1) 100%);"
+        },
+        "* .MuiListItem-root": {
+          backgroundColor: "white"
+        },
         mode: "dark"
       }
     })}
