691bda817ac9dfbdf64a1fc68e3cecc2c8ffec91
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2022-01-09 11:14:50 +0530

    moved subcomponents out of ContentSection

--- src/content.js
+++ src/content.js
@@ -19,68 +19,68 @@ import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import { StandardButton, RepeatButton } from './button';
 import Breadcrumbs from './breadcrumbs';
 
-export default function ContentSection(props) {
-  const VideoProgressBar = styled(LinearProgress)(({ theme }) => ({
-    [`&.${linearProgressClasses.colorPrimary}`]: {
-      backgroundColor: "#303234"
-    },
-    [`& .${linearProgressClasses.bar}`]: {
-      background: "linear-gradient(180deg, rgba(255,0,107,1) 0%, rgba(165,0,243,1) 100%);",
-    }
-  }))
-
-  function VideoCard(props) {
-    // Represents a list item in the list of course videos
-
-    function VideoThumbnail(props) {
-      // Represents a video thumbnail within a VideoCard
-
-      return (
-        <div style={{ marginRight: "2em", display: "flex", flexDirection: "column" }}>
-          <div className="video-thumbnail-container">
-            <img className="video-thumbnail" alt="thumbnail" src={props.img} />
-            <span className="video-icon">
-              {
-                (props.completed) ?
-                  <CheckCircleIcon color="success" fontSize="large" /> :
-                  <PlayArrowIcon />
-              }
-            </span>
-            <span className="video-time">40:08</span>
-          </div>
-          <VideoProgressBar variant="determinate" value={(props.completed) ? 100 : 50} />
-        </div>
-      );
-    }
-
-    return (
-      <ListItem className="video-card-list-item">
-        <VideoThumbnail completed={props.completed} img={props.img} />
-        <ListItemText style={{ maxWidth: "20em" }}>
-          <b>Day {props.index} | {props.title}</b> <br /> {props.description}
-        </ListItemText>
-        {
-          (props.completed) ?
-            <RepeatButton startIcon={<PlayArrowIcon />}>Watch Again</RepeatButton> :
-            <StandardButton>Resume</StandardButton>
-        }
-      </ListItem>
-    );
+const VideoProgressBar = styled(LinearProgress)(({ theme }) => ({
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor: "#303234"
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    background: "linear-gradient(180deg, rgba(255,0,107,1) 0%, rgba(165,0,243,1) 100%);",
   }
+}))
 
-  function Participants(props) {
+function VideoCard(props) {
+  // Represents a list item in the list of course videos
+
+  function VideoThumbnail(props) {
+    // Represents a video thumbnail within a VideoCard
+
     return (
-      <div style={{ display: "flex", alignItems: "center", marginTop: "1em" }}>
-        <AvatarGroup max={3} className="watching-group">
-          <Avatar style={{ width: "1.5em", height: "1.5em" }} src="stretching.jpg" />
-          <Avatar style={{ width: "1.5em", height: "1.5em" }} src="stretching.jpg" />
-          <Avatar style={{ width: "1.5em", height: "1.5em" }} src="stretching.jpg" />
-        </AvatarGroup>
-        <span style={{ marginLeft: "1em" }}><em>You and 50+ others are here</em></span>
+      <div style={{ marginRight: "2em", display: "flex", flexDirection: "column" }}>
+        <div className="video-thumbnail-container">
+          <img className="video-thumbnail" alt="thumbnail" src={props.img} />
+          <span className="video-icon">
+            {
+              (props.completed) ?
+                <CheckCircleIcon color="success" fontSize="large" /> :
+                <PlayArrowIcon />
+            }
+          </span>
+          <span className="video-time">40:08</span>
+        </div>
+        <VideoProgressBar variant="determinate" value={(props.completed) ? 100 : 50} />
       </div>
     );
   }
 
+  return (
+    <ListItem className="video-card-list-item">
+      <VideoThumbnail completed={props.completed} img={props.img} />
+      <ListItemText style={{ maxWidth: "20em" }}>
+        <b>Day {props.index} | {props.title}</b> <br /> {props.description}
+      </ListItemText>
+      {
+        (props.completed) ?
+          <RepeatButton startIcon={<PlayArrowIcon />}>Watch Again</RepeatButton> :
+          <StandardButton>Resume</StandardButton>
+      }
+    </ListItem>
+  );
+}
+
+function Participants(props) {
+  return (
+    <div style={{ display: "flex", alignItems: "center", marginTop: "1em" }}>
+      <AvatarGroup max={3} className="watching-group">
+        <Avatar style={{ width: "1.5em", height: "1.5em" }} src="stretching.jpg" />
+        <Avatar style={{ width: "1.5em", height: "1.5em" }} src="stretching.jpg" />
+        <Avatar style={{ width: "1.5em", height: "1.5em" }} src="stretching.jpg" />
+      </AvatarGroup>
+      <span style={{ marginLeft: "1em" }}><em>You and 50+ others are here</em></span>
+    </div>
+  );
+}
+
+export default function ContentSection(props) {
   return (
     <Box component="main" sx={{ textAlign: "justify", margin: "4em", display: "block" }}>
 
